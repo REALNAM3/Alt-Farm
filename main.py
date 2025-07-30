2ef476f022715b9b0b59a0eec821a0c6e104d18a
REALNAM3 <marckgamer1357@gmail.com>
2025-07-29 20:16:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def modson(interaction: discord.Interaction):
                             else:
                                 username = "Unknown"
                         if presence_info["userPresenceType"] == 2:
-                            mod_lines.append(f"```diff\n[In Game]: {username} \n```")
+                            mod_lines.append(f"```diff\n+[In Game]: {username} \n```")
                         else:
                             mod_lines.append(f"```ini\n[Online]: {username}\n```")
                         any_found = True
